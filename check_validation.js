4622c2778ba1a4aded84f081db12079d76190c28
cygas <35080448+cygas@users.noreply.github.com>
2018-06-22 13:06:59 +0200

    Add files via upload

--- check_validation.js
+++ check_validation.js
@@ -1,4 +1,21 @@
 (function(global){
+	
+	global.Validator = {};
+	
+	let checkName = function(obj){
+		return obj.name != undefined;			
+	};
+	let checkUser = function(obj){
+		return obj.username != undefined;			
+	};
+	let checkId = function(obj){
+		return obj.id != "";		
+	};	
+	Validator.checkName = checkName;
+	Validator.checkUser = checkUser;
+	Validator.checkId = checkId;
+	
+	/*
 	global.checkName = function(form){
 		return form.obj.name != undefined;			
 	};
@@ -8,4 +25,27 @@
 	global.checkId = function(form){
 		return form.obj.id != "";		
 	};	
+	*/
+	
+	/*
+		global.Validator = {};
+		Validator.checkName = function(){}
+		
+		let checkName = function(){}
+		Validator.checkName = checkName
+		
+		Validator.prototype.checkName = checkName
+		
+		global.Check = function(form){
+		this.checkName = function(form){
+			return form.obj.name != undefined;			
+		};
+		this.checkUser = function(form){
+			return form.obj.username != undefined;			
+		};
+		this.checkId = function(form){
+			return form.obj.id != "";		
+		};	
+	};
+	*/
 })(this);
